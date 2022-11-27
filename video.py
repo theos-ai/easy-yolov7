e71c0c7a3ab0e70e63e6ce58b8a3142996baf9d2
ramacastro <ramicasmar@gmail.com>
2022-11-27 15:01:12 +0100

    Fix

--- video.py
+++ video.py
@@ -6,13 +6,13 @@ import cv2
 yolov7 = YOLOv7()
 yolov7.load('coco.weights', classes='coco.yaml', device='cpu') # use 'gpu' for CUDA GPU inference
 
-video = cv2.VideoCapture('video.mp4')
+video = cv2.VideoCapture('messi.mp4')
 width  = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = int(video.get(cv2.CAP_PROP_FPS))
 frames_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-output = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
+output = cv2.VideoWriter('video.mp4', fourcc, fps, (width, height))
 
 if video.isOpened() == False:
 	print('[!] error opening the video')
@@ -20,15 +20,18 @@ if video.isOpened() == False:
 print('[+] detecting video...\n')
 pbar = tqdm(total=frames_count, unit=' frames', dynamic_ncols=True, position=0, leave=True)
 
-while video.isOpened():
-    ret, frame = video.read()
-    if ret == True:
-        detections = yolov7.detect(frame)
-        detected_frame = draw(frame, detections)
-        output.write(detected_frame)
-        pbar.update(1)
-    else:
-        break
+try:
+    while video.isOpened():
+        ret, frame = video.read()
+        if ret == True:
+            detections = yolov7.detect(frame)
+            detected_frame = draw(frame, detections)
+            output.write(detected_frame)
+            pbar.update(1)
+        else:
+            break
+except KeyboardInterrupt:
+    pass
 
 pbar.close()
 video.release()
